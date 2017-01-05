f0573fd1f18ad0923e6bc0c8a161d9f0a90d659c
Ashish Kapoor <swiftobjc@gmail.com>
2017-01-05 19:17:12 +0530

    Able to scan, list and stop scanning devices. Also, added code to make connection

--- iot-ble/ViewController.swift
+++ iot-ble/ViewController.swift
@@ -12,7 +12,6 @@ import CoreBluetooth
 class ViewController: UIViewController, CBCentralManagerDelegate {
 
     var centralManager: CBCentralManager!
-    
     var peripheral: CBPeripheral!
 
     override func viewDidLoad() {
@@ -22,38 +21,46 @@ class ViewController: UIViewController, CBCentralManagerDelegate {
     
     // Scan Receive the update of central manager’s state. **Required**
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        print("state: \(central.state)")
+        print("State of the Bluetooth: \(central.state)\n")
         centralManager.scanForPeripherals(withServices:nil, options: nil)
-
     }
     
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
-        print("peripheral: \(peripheral)")
+        print("peripheral's Name: \(peripheral.name!)")
+        print("peripheral's Identifier: \(peripheral.identifier)\n")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        centralManager.connect(peripheral, options: nil)
+    }
+    
+    // Called when it succeeded
+    func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
+        print("connected!")
+    }
+    
+    // Called when it failed
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print("failed…")
     }
     
     func centralManagerDidUpdateState(central: CBCentralManager!){
         print("CentralManager is initialized")
         
         switch central.state {
-            
         case CBManagerState.unauthorized:
             print("The app is not authorized to use Bluetooth low energy.")
-            
         case CBManagerState.poweredOff:
             print("Bluetooth is currently powered off.")
-            
         case CBManagerState.poweredOn:
             print("Bluetooth is currently powered on and available to use.")
-
             centralManager.scanForPeripherals(withServices: nil, options: nil)
-
         default:break
         }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
